f697bf82bbf5d47c99af61e395b0927259833f0f
Harley Rayner <harleymteden@gmail.com>
2018-09-11 21:12:25 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,9 +10,10 @@ class Patient
   end
 
   def appointments
-    Appointment.all.collect {  |appointment|
+    Appointment.all.collect do |appointment|
       binding.pry
-      self.appointment }
+      self.appointment
+     end
   end
 
   def new_appointment(doctor, date)
